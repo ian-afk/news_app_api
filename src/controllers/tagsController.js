c3feff4f7592fa8c873d2d3b13ef30656b318294
ian-afk <68414797+ian-afk@users.noreply.github.com>
2025-03-03 10:04:38 +0800

    add page limitation for infinite scrolling for news by tags

--- src/controllers/tagsController.js
+++ src/controllers/tagsController.js
@@ -32,12 +32,26 @@ export const getListTags = async (req, res) => {
 
 export const getNewsByTag = async (req, res) => {
   try {
+    const { page = 1, limit = 10 } = req.query;
     const { tagId } = req.params; // Get tagId from request URL
+
+    const totalNews = await News.countDocuments({
+      tags: { $in: [tagId] },
+    });
+
     const news = await News.find({
       tags: { $in: [new mongoose.Types.ObjectId(tagId)] },
-    }).populate("tags");
-
-    res.status(200).json({ success: true, docs: news });
+    })
+      .populate("tags")
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit));
+    const totalPages = Math.ceil(totalNews / limit);
+    res.status(200).json({
+      success: true,
+      docs: news,
+      page: Number(page),
+      totalPages,
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
